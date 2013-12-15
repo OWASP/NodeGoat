6ae9e68abfa41b4b0534d3929e882810f30f03bd
Chetan Karande <kchetan.tech@gmail.com>
2013-12-15 16:11:00 -0500

    Use double quotes for strings

--- app/data/session-dao.js
+++ app/data/session-dao.js
@@ -17,7 +17,7 @@ function SessionDAO(db) {
         // Generate session id
         var current_date = (new Date()).valueOf().toString();
         var random = Math.random().toString();
-        var session_id = crypto.createHash('sha1').update(current_date + random).digest("hex");
+        var session_id = crypto.createHash("sha1").update(current_date + random).digest("hex");
 
         // Create session document
         var session = {
