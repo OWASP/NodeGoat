10cc6b06dedfd1a90bc45d1eb5d2f469b876e70b
Chetan Karande <kchetan.tech@gmail.com>
2014-05-06 15:49:25 -0400

    Use MONGODB_URL from env variable if provided

--- config/env/all.js
+++ config/env/all.js
@@ -1,7 +1,7 @@
 // default app configuration
 var defaultConfig = {
     port: process.env.PORT || 4000,
-    db: "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
+    db: process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256"
