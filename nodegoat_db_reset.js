14ec7dbf7c8154780cae60aedeab91f03011efc5
Chetan Karande <kchetan.tech@gmail.com>
2014-05-05 23:25:19 -0400

    Update passwords

--- nodegoat_db_reset.js
+++ nodegoat_db_reset.js
@@ -14,7 +14,8 @@ db.users.insert([{
 	"userName" : "admin",
 	"firstName" : "Node Goat",
 	"lastName" : "Admin ",
-	"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", //Admin_123
+    "password": "Admin_123",
+	//"password" : "$2a$10$8Zo/1e8KM8QzqOKqbDlYlONBOzukWXrM.IiyzqHRYDXqwB3gzDsba", // Admin_123
 	"userId" : 1,
     "isAdmin": true
 },
@@ -22,16 +23,18 @@ db.users.insert([{
 	"userName" : "user1",
 	"firstName" : "John",
 	"lastName" : "Doe",
-    benefitStartDate: "2030-01-10",
-	"password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
+    "benefitStartDate": "2030-01-10",
+    "password": "User1_123",
+	// "password" : "$2a$10$RNFhiNmt2TTpVO9cqZElb.LQM9e1mzDoggEHufLjAnAKImc6FNE86",// User1_123
 	"userId" : 2
 },
 {
 	"userName" : "user2",
 	"firstName" : "Will",
 	"lastName" : "Smith",
-    benefitStartDate: "2025-11-30",
-	"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
+    "benefitStartDate": "2025-11-30",
+    "password" : "User2_123",
+	//"password" : "$2a$10$Tlx2cNv15M0Aia7wyItjsepeA8Y6PyBYaNdQqvpxkIUlcONf1ZHyq", // User2_123
 	"userId" : 3
 }]);
 
