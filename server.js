29e736342858754d78293ddd41543b6d9a2e096e
Chetan Karande <kchetan.tech@gmail.com>
2015-01-23 12:20:28 -0500

    Upgrade to Express 4.x

--- server.js
+++ server.js
@@ -3,28 +3,32 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 
 var express = require("express");
-var app = express(); // Web framework to handle routing requests
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var consolidate = require("consolidate"); // Templating library adapter for Express
 var swig = require("swig");
 var helmet = require("helmet");
 var MongoClient = require("mongodb").MongoClient; // Driver for connecting to MongoDB
+var http = require("http");
 
+var app = express(); // Web framework to handle routing requests
 var routes = require("./app/routes");
 var config = require("./config/config"); // Application config properties
 
-var http = require("http");
 
 /*
-// Fix for A6-Sensitive Data Exposure
-// Load keys for establishing secure HTTPS connection
-var fs = require("fs");
-var https = require("https");
-var path = require("path");
-var httpsOptions = {
-    key: fs.readFileSync(path.resolve(__dirname, "./app/cert/key.pem")),
-    cert: fs.readFileSync(path.resolve(__dirname, "./app/cert/cert.pem"))
-};
-*/
+ // Fix for A6-Sensitive Data Exposure
+ // Load keys for establishing secure HTTPS connection
+ var fs = require("fs");
+ var https = require("https");
+ var path = require("path");
+ var httpsOptions = {
+ key: fs.readFileSync(path.resolve(__dirname, "./app/cert/key.pem")),
+ cert: fs.readFileSync(path.resolve(__dirname, "./app/cert/cert.pem"))
+ };
+ */
 
 MongoClient.connect(config.db, function(err, db) {
 
@@ -32,74 +36,51 @@ MongoClient.connect(config.db, function(err, db) {
 
     if (err) throw err;
 
-    /*
-     //Fix for A5 - Security MisConfig
-     // Remove default x-powered-by response header
-     app.disable("x-powered-by");
-
-     // Prevent opening page in frame or iframe to protect from clickjacking
-     app.use(helmet.xframe());
-
-     // Prevents browser from caching and storing page
-     app.use(helmet.cacheControl());
-
-     // Allow loading resources only from white-listed domains
-     app.use(helmet.csp());
-
-     // Allow communication only on HTTPS
-     app.use(helmet.hsts());
-
-     // Enable XSS filter in IE (On by default)
-     app.use(helmet.iexss());
-
-     // Forces browser to only use the Content-Type set in the response header instead of sniffing or guessing it
-     app.use(helmet.contentTypeOptions());
-     */
-
-    // Adding/ remove HTTP Headers for security
-    app.use(express.favicon());
-
     // Express middleware to populate "req.body" so we can access POST variables
-    app.use(express.json());
-    app.use(express.urlencoded());
+    // parse application/json
+    app.use(bodyParser.json());
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({extended: false}));
 
     // Express middleware to populate "req.cookies" so we can access cookies
-    app.use(express.cookieParser());
+    app.use(cookieParser());
 
     // Enable session management using express middleware
-    app.use(express.session({
+    app.use(session({
+        resave: true,
+        saveUninitialized: true,
         secret: config.cookieSecret
         /*
-        //Fix for A5 - Security MisConfig
-        // Use generic cookie name
-        key: "sessionId",
+         //Fix for A5 - Security MisConfig
+         // Use generic cookie name
+         key: "sessionId",
 
-        //Fix for A3 - XSS
-        cookie: {
-            httpOnly: true,
-            secure: true
-        }
-        */
+         //Fix for A3 - XSS
+         cookie: {
+         httpOnly: true,
+         secure: true
+         }
+         */
     }));
     /* Fix for A8 - CSRF
-    //Enable Express csrf protection
-    app.use(express.csrf());
+     //Enable Express csrf protection
+     app.use(express.csrf());
 
-    // Make csrf token available in templates
-    app.use(function(req, res, next) {
-        res.locals.csrftoken = req.csrfToken();
-        next();
-    });
-    */
+     // Make csrf token available in templates
+     app.use(function(req, res, next) {
+     res.locals.csrftoken = req.csrfToken();
+     next();
+     });
+     */
 
     // Register templating engine
     app.engine(".html", consolidate.swig);
     app.set("view engine", "html");
     app.set("views", __dirname + "/app/views");
     app.use(express.static(__dirname + "/app/assets"));
+    app.use(favicon(__dirname + "/app/assets/favicon.ico"));
 
     // Application routes
-    app.use(app.router);
     routes(app, db);
 
     swig.init({
@@ -107,9 +88,9 @@ MongoClient.connect(config.db, function(err, db) {
         // Autoescape disabled
         autoescape: false
         /*
-        // Fix for A3 - XSS, enable auto escaping
-        autoescape: true //default value
-        */
+         // Fix for A3 - XSS, enable auto escaping
+         autoescape: true //default value
+         */
     });
 
     // Insecure HTTP connection
@@ -117,11 +98,11 @@ MongoClient.connect(config.db, function(err, db) {
         console.log("Express http server listening on port " + config.port);
     });
     /*
-    // Fix for A6-Sensitive Data Exposure
-    // Use secure HTTPS protocol
-    https.createServer(httpsOptions, app).listen(config.port, function() {
-        console.log("Express https server listening on port " + config.port);
-    });
-    */
+     // Fix for A6-Sensitive Data Exposure
+     // Use secure HTTPS protocol
+     https.createServer(httpsOptions, app).listen(config.port, function() {
+     console.log("Express https server listening on port " + config.port);
+     });
+     */
 
 });
