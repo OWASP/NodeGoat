8d2fb481868ec03fdef635b144a73a4506526d7d
Chetan Karande <kchetan.tech@gmail.com>
2015-06-27 08:25:04 -0400

    Add Heroku deploy button

--- config/env/all.js
+++ config/env/all.js
@@ -1,7 +1,7 @@
 // default app configuration
 module.exports = {
     port: process.env.PORT || 4000,
-    db: process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
+    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URL || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256"
