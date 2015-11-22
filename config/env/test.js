37f2eb8e6c3c3834fd2891963aacfc1a0ad84de6
Kim Carter <Kim.Carter@binarymist.net>
2015-11-22 16:05:02 +1300

    Added Zap API feedback speed to config. Added ignore of Zap report files. Provided some more documentation around mongodb in config.

--- config/env/test.js
+++ config/env/test.js
@@ -1,4 +1,5 @@
-module.exports = {    
+module.exports = {
     zapHostName: "192.168.56.20",
-    zapPort: "8080"
+    zapPort: "8080",
+    zapApiFeedbackSpeed: 5000 // Milliseconds.
 };
--- test/security/profile-test.js
+++ test/security/profile-test.js
@@ -218,7 +218,7 @@ test.describe(zapTargetAppRoute + " regression test suite", function () {
                         });                    
                         
                      }
-                  }, 10000);
+                  }, config.zapApiFeedbackSpeed);
 
             });
          }
