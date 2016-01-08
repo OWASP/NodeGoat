216b705835741a465e415f9015ffeb7540f224f1
Chetan Karande <kchetan.tech@gmail.com>
2016-01-08 16:08:39 +0530

    Update Host property

--- config/env/all.js
+++ config/env/all.js
@@ -5,5 +5,5 @@ module.exports = {
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256",
-    hostName: process.env.HOST || "localhost"
+    hostName: "localhost"
 };
