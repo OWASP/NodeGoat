47ec1abb7d5398f09f755c2688d2577e63d59634
Matt Everson <meverson@tenable.com>
2016-03-10 22:49:10 -0600

    Update travis script

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup npm start &
+npm start &
 docker run -u zap -i owasp/zap2docker-stable zap-cli quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
