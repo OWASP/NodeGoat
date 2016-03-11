81c6f7a7be778a2b0df499877efe11372f425811
Matt Everson <meverson@tenable.com>
2016-03-10 23:31:06 -0600

    Add curl test

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npm start &
+curl http://localhost:4000/login
 docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
