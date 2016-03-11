a3a2ad2b9302cdc74ade330ba1c809cb88f19544
Matt Everson <meverson@tenable.com>
2016-03-10 23:38:17 -0600

    Slow down testing slightly to allow for start

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 npm start &
+sleep 5
 curl http://localhost:4000/login
 docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
