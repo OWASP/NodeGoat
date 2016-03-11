9c406cb5d1010cbd3bd9f9004346d1c1f08ccdfd
Matt Everson <meverson@tenable.com>
2016-03-10 23:43:58 -0600

    Switch to ip address

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm start &
 sleep 5
-curl http://localhost:4000/login
-docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
+curl http://127.0.0.1:4000/login
+docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://127.0.0.1:4000
