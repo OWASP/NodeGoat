53e883c4e3bff3011a858d4732cfe0cb02c72624
Matt Everson <meverson@tenable.com>
2016-03-11 09:27:24 -0600

    Update zap script to use docker bridge IP

--- .travis-script.sh
+++ .travis-script.sh
@@ -2,4 +2,4 @@
 npm start &
 sleep 5
 curl http://127.0.0.1:4000/login
-docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" --spider -r http://127.0.0.1:4000
+docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" --spider -r http://172.17.0.1:4000
