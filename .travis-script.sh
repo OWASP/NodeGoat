6b3fa6979d96a43c360f0196b72f2b9869ecba33
Matt Everson <meverson@tenable.com>
2016-03-10 22:58:58 -0600

    Increase verbosity

--- .travis-script.sh
+++ .travis-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 npm start &
-docker run -u zap -i owasp/zap2docker-stable zap-cli quick-scan -v --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
+docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" -s xss,sqli --spider http://localhost:4000
