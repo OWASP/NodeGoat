4e6b48543fd5912a65fc4c7662c30fbc1f4588bf
Matt Everson <meverson@tenable.com>
2016-03-11 11:09:15 -0600

    Look up docker bridge IP in travis infrastructure

--- .travis-script.sh
+++ .travis-script.sh
@@ -2,4 +2,5 @@
 npm start &
 sleep 5
 curl http://127.0.0.1:4000/login
-docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" --spider -r http://172.17.0.1:4000
+hostip = docker run -u zap -it owasp/zap2docker-stable bash -c 'ip route show' | awk '/default/ {print$3}'
+docker run -u zap -i owasp/zap2docker-stable zap-cli --verbose quick-scan --self-contained -o "-config api.disablekey=true" --spider -r http://$hostip:4000
