52e51e910f50ff39a8959019e8ac983328b230a6
Chetan Karande <chetan.yjc@gmail.com>
2016-11-20 10:19:48 -0500

    Change sample DB host from mongohq to mongolab

--- config/env/all.js
+++ config/env/all.js
@@ -1,7 +1,7 @@
 // default app configuration
 module.exports = {
     port: process.env.PORT || 4000,
-    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URI || "mongodb://nodegoat:owasp@widmore.mongohq.com:10000/nodegoat",
+    db:  process.env.MONGOLAB_URI || process.env.MONGODB_URI || "mongodb://nodegoat:owasp@ds159217.mlab.com:59217/nodegoat",
     cookieSecret: "session_cookie_secret_key_here",
     cryptoKey: "a_secure_key_for_crypto_here",
     cryptoAlgo: "aes256",
