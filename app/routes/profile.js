2d4ceeec26244a19f293393c78ad6e42624bbf28
Liran Tal <liran.tal@gmail.com>
2017-02-05 20:10:11 +0200

    New section: ReDoS vulnerableilities Added a new section to the tutorial, which includes a real vulnerability in the NodeGoat application too. This new section introduces a vulnreable form which validates input data based on a regex pattern which is exploitable and can lead to catastrophic backtracking, rendering the Node.js appp completely hanging and unable to serve new requests (as well as taking up 100% cpu)
    * Updated profile form to require the 'Bank Routing' text input field to be in a specific format
    * Updated profile form submission to validate the input form to a specific pattern (which is vulnerable)
    * Updated tutorial to include information about this vulnerability

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -27,6 +27,21 @@ function ProfileHandler(db) {
         var bankAcc = req.body.bankAcc;
         var bankRouting = req.body.bankRouting;
 
+        // Fix for Section: ReDoS attack
+        // The following regexPattern that is used to validate the bankRouting number is insecure and vulnerable to
+        // catastrophic backtracking which means that specific type of input may cause it to consume all CPU resources
+        // with an exponential time until it completes
+        var regexPattern = /([0-9]+)+C/;
+        // Allow only numbers with a suffix of the letter C, for example: 'XXXXXXC'
+        var testComplyWithRequirements = regexPattern.test(bankRouting)
+        // if the regex test fails we do not allow saving
+        if (testComplyWithRequirements !== true) {
+          return res.render("profile", {
+            updateError: 'Bank Routing number does not comply with requirements for format specified'
+            }
+          )
+        }
+
         var userId = req.session.userId;
 
         profile.updateUser(
