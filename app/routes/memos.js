9fd0b1ad41c38468ec400c14588ac3c6b01fdc2c
Liran Tal <liran.tal@nielsen.com>
2017-02-17 15:40:22 +0200

    fixed #9 - userId variable isnt set when viewing the memos page

--- app/routes/memos.js
+++ app/routes/memos.js
@@ -5,25 +5,26 @@ function MemosHandler(db) {
 
   var memosDAO = new MemosDAO(db);
 
+  var self = this;
   this.addMemos = function(req, res, next) {
+
     memosDAO.insert(req.body.memo, function(err, docs) {
       if (err) return next(err);
 
-      memosDAO.getAllMemos(function(err, docs) {
-        if (err) return next(err);
-        return res.render("memos", {
-          memosList: docs
-        });
-      });
+      self.displayMemos(req, res, next)
 
     });
   };
 
   this.displayMemos = function(req, res, next) {
+
+    var userId = req.session.userId;
+
     memosDAO.getAllMemos(function(err, docs) {
       if (err) return next(err);
       return res.render("memos", {
-        memosList: docs
+        memosList: docs,
+        userId: userId
       });
     });
   };
