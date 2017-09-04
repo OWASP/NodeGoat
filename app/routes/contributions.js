4d4ec1920f981ebce0789042185b6518f1112c94
susanstdemos <19533107+susanstdemos@users.noreply.github.com>
2017-09-04 15:53:40 -0400

    Update contributions.js

--- app/routes/contributions.js
+++ app/routes/contributions.js
@@ -23,7 +23,7 @@ function ContributionsHandler(db) {
         // Insecure use of eval() to parse inputs
       
        var preTax = eval(req.body.preTax);
-        //var afterTax = eval(req.body.afterTax);
+       var afterTax = eval(req.body.afterTax);
        // var roth = eval(req.body.roth);
       
         
