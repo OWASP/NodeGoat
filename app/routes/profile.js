e179f21ad317f76719186f3f92eb7e53d0d63abe
Berkley Horan <horans11@gmail.com>
2019-05-16 08:38:15 -0600

    fixed a few things

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -46,7 +46,7 @@ function ProfileHandler(db) {
         // --
         // The Fix: Instead of using greedy quantifiers the same regex will work if we omit the second quantifier +
         // var regexPattern = /([0-9]+)\#/;
-        var regexPattern = /([0-9]+)+\#/;
+        var regexPattern = /([0-9]+)\#/;
         // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
         var testComplyWithRequirements = regexPattern.test(bankRouting);
         // if the regex test fails we do not allow saving
