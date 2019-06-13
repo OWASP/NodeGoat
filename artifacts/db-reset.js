5eaeb5245780432293bc35b6a082bc69c6ff0902
KoolTheba <26543236+KoolTheba@users.noreply.github.com>
2019-06-13 12:15:59 +0200

    Added exception to manage wrong connection

--- artifacts/db-reset.js
+++ artifacts/db-reset.js
@@ -44,7 +44,6 @@ function parseResponse(err, res, comm) {
         console.log("ERROR:");
         console.log(comm);
         console.log(JSON.stringify(err));
-
         process.exit(1);
     }
     console.log(comm);
@@ -59,6 +58,7 @@ MongoClient.connect(config.db, function(err, db) {
     if (err) {
         console.log("ERROR: connect");
         console.log(JSON.stringify(err));
+        process.exit(1);
     }
     console.log("Connected to the database: " + config.db);
 
