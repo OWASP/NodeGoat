e9c53caa79008f3fcf47a7d07525da9e59158985
ulises Gascon <ulisesgascongonzalez@gmail.com>
2019-06-14 10:28:38 +0200

    Linter test specs

--- cypress/integration/learn_spec.js
+++ cypress/integration/learn_spec.js
@@ -1,24 +1,22 @@
 describe('/learn behaviour', () => {
-  
-    afterEach(() => {
-      cy.visitPage('/logout')
-    })
-  
-    it('Should redirect if the user has not logged in', () => {
-      cy.visitPage('/learn?url=/dashboard')
-      cy.url().should('include', 'login')
-    })
-  
-    it('Should be accesible if the user is an admin', () => {
-      cy.adminSignIn()
-      cy.visitPage('/learn?url=/dashboard')
-      cy.url().should('include', 'dashboard')
-    })
-  
-    it('Should be accesible if the user is not an admin', () => {
-      cy.userSignIn()
-      cy.visitPage('/learn?url=/dashboard')
-      cy.url().should('include', 'dashboard')
-    })
+  afterEach(() => {
+    cy.visitPage('/logout')
+  })
 
+  it('Should redirect if the user has not logged in', () => {
+    cy.visitPage('/learn?url=/dashboard')
+    cy.url().should('include', 'login')
+  })
+
+  it('Should be accesible if the user is an admin', () => {
+    cy.adminSignIn()
+    cy.visitPage('/learn?url=/dashboard')
+    cy.url().should('include', 'dashboard')
+  })
+
+  it('Should be accesible if the user is not an admin', () => {
+    cy.userSignIn()
+    cy.visitPage('/learn?url=/dashboard')
+    cy.url().should('include', 'dashboard')
+  })
 })
--- cypress/integration/research_spec.js
+++ cypress/integration/research_spec.js
@@ -1,50 +1,48 @@
 describe('/research behaviour', () => {
-  
-    afterEach(() => {
-        cy.visitPage('/logout')
-    })
+  afterEach(() => {
+    cy.visitPage('/logout')
+  })
 
-    it('Should redirect if the user has not logged in', () => {
-      cy.visitPage('/research')
-      cy.url().should('include', 'login')
-    })
-  
-    it('Should be accesible if the user is an admin', () => {
-      cy.adminSignIn()
-      cy.visitPage('/research')
-      cy.url().should('include', 'research')
-    })
-  
-    it('Should be accesible if the user is not an admin', () => {
-      cy.userSignIn()
-      cy.visitPage('/research')
-      cy.url().should('include', 'research')
-    })
+  it('Should redirect if the user has not logged in', () => {
+    cy.visitPage('/research')
+    cy.url().should('include', 'login')
+  })
 
-    it('Should be a form with an input', () => {
-        cy.userSignIn()
-        cy.visitPage('/research')
-        cy.get('form[role="search"]')
-          .find('input')
-    })
+  it('Should be accesible if the user is an admin', () => {
+    cy.adminSignIn()
+    cy.visitPage('/research')
+    cy.url().should('include', 'research')
+  })
 
-    it('Should have an input text as a valid stock symbol', () => {
-        const stockSymbol = "AAPL"
-        cy.adminSignIn()
-        cy.visitPage('/research')
-        cy.get('.form-control')
-          .clear()
-          .type(stockSymbol)
-  
-        cy.get('form')
-          .should('have.attr', 'action', '/research')
-          .invoke('attr', 'action', '/skip')
+  it('Should be accesible if the user is not an admin', () => {
+    cy.userSignIn()
+    cy.visitPage('/research')
+    cy.url().should('include', 'research')
+  })
 
-        cy.get('button[type="submit"]')
-          .first()
-          .click()
+  it('Should be a form with an input', () => {
+    cy.userSignIn()
+    cy.visitPage('/research')
+    cy.get('form[role="search"]')
+      .find('input')
+  })
 
-        cy.url().should('include', 'https%3A%2F%2Ffinance.yahoo.com%2Fquote%2F&symbol=AAPL')
-      })
+  it('Should have an input text as a valid stock symbol', () => {
+    const stockSymbol = 'AAPL'
+    cy.adminSignIn()
+    cy.visitPage('/research')
+    cy.get('.form-control')
+      .clear()
+      .type(stockSymbol)
+
+    cy.get('form')
+      .should('have.attr', 'action', '/research')
+      .invoke('attr', 'action', '/skip')
+
+    cy.get('button[type="submit"]')
+      .first()
+      .click()
+
+    cy.url().should('include', 'https%3A%2F%2Ffinance.yahoo.com%2Fquote%2F&symbol=AAPL')
+  })
 })
-
