39b1e845b10c302fdd059ce2458525b6aae58f2f
Thom Mondeaux <Thommond554@gmail.com>
2020-03-09 15:03:11 -0400

    I edited the allocations.html and allocations-dao.js . The new additons will prevent SSJS attacks by putting a specific critera which can be inputed into to program without error or denial of service.

--- app/data/allocations-dao.js
+++ app/data/allocations-dao.js
@@ -61,20 +61,19 @@ function AllocationsDAO(db) {
         function searchCriteria() {
 
             if (threshold) {
-                /*
                 // Fix for A1 - 2 NoSQL Injection - escape the threshold parameter properly
                 // Fix this NoSQL Injection which doesn't sanitze the input parameter 'threshold' and allows attackers
                 // to inject arbitrary javascript code into the NoSQL query:
                 // 1. 0';while(true){}'
                 // 2. 1'; return 1 == '1
-                // Also implement fix in allocations.html for UX.                             
+                // Also implement fix in allocations.html for UX.
                 const parsedThreshold = parseInt(threshold, 10);
-                
+
                 if (parsedThreshold >= 0 && parsedThreshold <= 99) {
                     return {$where: `this.userId == ${parsedUserId} && this.stocks > ${threshold}`};
                 }
                 throw `The user supplied threshold: ${parsedThreshold} was not valid.`;
-                */
+
                 return {
                     $where: `this.userId == ${parsedUserId} && this.stocks > '${threshold}'`
                 };
