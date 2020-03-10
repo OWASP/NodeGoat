2159194fb1c6d2164284cff2e5dfda3ae03af7b4
Thom Mondeaux <Thommond554@gmail.com>
2020-03-10 12:57:41 -0400

    I made the sessions secure and they will end.

--- app/routes/session.js
+++ app/routes/session.js
@@ -107,13 +107,18 @@ function SessionHandler(db) {
             // Fix the problem by regenerating a session in each login
             // by wrapping the below code as a function callback for the method req.session.regenerate()
             // i.e:
-            // `req.session.regenerate(function() {})`
-            req.session.userId = user._id;
-            if (user.isAdmin) {
-              return res.redirect("/benefits");
-            } else {
-              return res.redirect("/dashboard");
-            }
+            req.session.regenerate(function() {
+
+              req.session.userId = user._id;
+
+              if (user.isAdmin) {
+                return res.redirect("/benefits");
+              } else {
+                return res.redirect("/dashboard");
+              }
+
+            })
+
         });
     };
 
