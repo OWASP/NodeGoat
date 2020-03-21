49493d99f9e28bc812e3b70bf2f233f2bf5d2c9d
Theba Gomez <gomez.teba@gmail.com>
2020-03-21 18:54:48 +0100

    refactor(react api): ES6 migration
    Related #192

--- apps/react-apirest/api/app.js
+++ apps/react-apirest/api/app.js
@@ -1,26 +1,18 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/api');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', apiRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -28,14 +20,9 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({error});
 });
 
 module.exports = app;
--- apps/react-apirest/api/routes/api.js
+++ apps/react-apirest/api/routes/api.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+  res.json({msg: 'respond with a resource'});
 });
 
 module.exports = router;
