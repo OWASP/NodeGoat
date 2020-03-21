0efe7f09a1eaac521728c9f72b5d6c7fe844485a
Theba Gomez <gomez.teba@gmail.com>
2020-03-21 18:37:25 +0100

    refactor(react api): ES6 migration
    Related #192

--- apps/react-apirest/api/routes/index.js
+++ apps/react-apirest/api/routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
--- apps/react-apirest/api/routes/users.js
+++ apps/react-apirest/api/routes/users.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
