a24718b14ab27b45e14d9630bcb68c6c1df54c26
Theba Gomez <gomez.teba@gmail.com>
2020-03-22 12:09:28 +0100

    feat(react client): added linter to project
    - Related #192

---
+++ apps/react-apirest/client/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
