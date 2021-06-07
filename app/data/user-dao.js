5afce2c8e0bb6ae8063c152f000bb8fdbefb0793
Jan Taylor <jantaylor@users.noreply.github.com>
2021-06-06 21:51:01 -0600

    feat: use bcrypt instead of bcrypt-nodejs

--- app/data/user-dao.js
+++ app/data/user-dao.js
@@ -1,12 +1,17 @@
-const bcrypt = require("bcrypt-nodejs");
+const bcrypt = require("bcrypt");
+
+/* The number of Salts to generate for the hash, the more rounds, the longer it takes to hash & compare */
+const BCRYPT_SALT_ROUNDS = 10;
 
 /* The UserDAO must be constructed with a connected database object */
 function UserDAO(db) {
 
     "use strict";
 
-    /* If this constructor is called without the "new" operator, "this" points
-     * to the global object. Log a warning and call it correctly. */
+    /**
+     * If this constructor is called without the "new" operator, "this" points
+     * to the global object. Log a warning and call it correctly.
+     */
     if (false === (this instanceof UserDAO)) {
         console.log("Warning: UserDAO constructor called without 'new' operator");
         return new UserDAO(db);
@@ -14,7 +19,7 @@ function UserDAO(db) {
 
     const usersCol = db.collection("users");
 
-    this.addUser = (userName, firstName, lastName, password, email, callback) => {
+    this.addUser = async (userName, firstName, lastName, password, email, callback) => {
 
         // Create user document
         const user = {
@@ -22,12 +27,7 @@ function UserDAO(db) {
             firstName,
             lastName,
             benefitStartDate: this.getRandomFutureDate(),
-            password //received from request param
-            /*
-            // Fix for A2-1 - Broken Auth
-            // Stores password  in a safer way using one way encryption and salt hashing
-            password: bcrypt.hashSync(password, bcrypt.genSaltSync())
-            */
+            password: await this.hashPassword(password, BCRYPT_SALT_ROUNDS)
         };
 
         // Add email if set
@@ -51,28 +51,18 @@ function UserDAO(db) {
         const day = (Math.floor(Math.random() * 10) + today.getDay()) % 29;
         const month = (Math.floor(Math.random() * 10) + today.getMonth()) % 12;
         const year = Math.ceil(Math.random() * 30) + today.getFullYear();
-        return `${year}-${("0" + month).slice(-2)}-${("0" + day).slice(-2)}`
+        return `${year}-${("0" + month).slice(-2)}-${("0" + day).slice(-2)}`;
     };
 
-    this.validateLogin = (userName, password, callback) => {
-
-        // Helper function to compare passwords
-        const comparePassword = (fromDB, fromUser) => {
-            return fromDB === fromUser;
-            /*
-            // Fix for A2-Broken Auth
-            // compares decrypted password stored in this.addUser()
-            return bcrypt.compareSync(fromDB, fromUser);
-            */
-        }
-
+    this.validateLogin = async (userName, password, callback) => {
         // Callback to pass to MongoDB that validates a user document
-        const validateUserDoc = (err, user) => {
+        const validateUserDoc = async (err, user) => {
 
             if (err) return callback(err, null);
 
             if (user) {
-                if (comparePassword(password, user.password)) {
+                const isValidPassword = await this.comparePassword(password, user.password);
+                if (isValidPassword) {
                     callback(null, user);
                 } else {
                     const invalidPasswordError = new Error("Invalid password");
@@ -117,6 +107,28 @@ function UserDAO(db) {
                 new: true
             },
             (err, data) =>  err ? callback(err, null) : callback(null, data.value.seq));
+    };
+
+    /**
+     * Hashes the password and returns the hash.
+     * Returns a promise as per best-practice for webserver to not block event-loop.
+     */
+    this.hashPassword = (password, saltOrRounds) => {
+        return password;
+        // Fix for A2-1 - Broken Auth
+        // Stores password in a safer way using one way encryption and random salt hashing
+        return bcrypt.hash(password, saltOrRounds);
+    };
+
+    /**
+     * Compares the password with the hashed pssword and returns a Boolean.
+     * Returns a promise as per best-practice for webserver to not block event-loop.
+     */
+    this.comparePassword = (password, hashedPassword) => {
+        return password === hashedPassword;
+        // Fix for A2-1 - Broken Auth
+        // Compares plain text password with the hash stored in the db from this.addUser()
+        // return bcrypt.compare(password, hashedPassword);
     };
 }
 
