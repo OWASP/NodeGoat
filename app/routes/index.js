d6d8c04c4c753188d5d84ba9e98e18eec87ef4b2
veromarinelli-iram <95386782+veromarinelli-iram@users.noreply.github.com>
2021-12-14 22:40:55 -0300

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -54,12 +54,12 @@ const index = (app, db) => {
     app.post("/contributions", isLoggedIn, contributionsHandler.handleContributionsUpdate);
 
     // Benefits Page
-    app.get("/benefits", isLoggedIn, benefitsHandler.displayBenefits);
-    app.post("/benefits", isLoggedIn, benefitsHandler.updateBenefits);
-    /* Fix for A7 - checks user role to implement  Function Level Access Control
+    //app.get("/benefits", isLoggedIn, benefitsHandler.displayBenefits);
+    //app.post("/benefits", isLoggedIn, benefitsHandler.updateBenefits);
+    // Fix for A7 - checks user role to implement  Function Level Access Control
      app.get("/benefits", isLoggedIn, isAdmin, benefitsHandler.displayBenefits);
      app.post("/benefits", isLoggedIn, isAdmin, benefitsHandler.updateBenefits);
-     */
+    
 
     // Allocations Page
     app.get("/allocations/:userId", isLoggedIn, allocationsHandler.displayAllocations);
