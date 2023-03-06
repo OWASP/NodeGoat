600daebb7c3c4c43d0092384a56f0d594bf49fd6
Liran Tal <liran.tal@gmail.com>
2023-03-06 15:17:27 +0200

    feat: make memos personal and per-user

--- app/routes/memos.js
+++ app/routes/memos.js
@@ -1,37 +1,31 @@
 const MemosDAO = require("../data/memos-dao").MemosDAO;
-const {
-    environmentalScripts
-} = require("../../config/config");
+const { environmentalScripts } = require("../../config/config");
 
 function MemosHandler(db) {
-    "use strict";
+  "use strict";
 
-    const memosDAO = new MemosDAO(db);
+  const memosDAO = new MemosDAO(db);
 
-    this.addMemos = (req, res, next) => {
+  this.addMemos = (req, res, next) => {
+    const { userId } = req.session;
+    memosDAO.insert(req.body.memo, userId, (err, docs) => {
+      if (err) return next(err);
+      this.displayMemos(req, res, next);
+    });
+  };
 
-        memosDAO.insert(req.body.memo, (err, docs) => {
-            if (err) return next(err);
-            this.displayMemos(req, res, next);
-        });
-    };
+  this.displayMemos = (req, res, next) => {
+    const { userId } = req.session;
 
-    this.displayMemos = (req, res, next) => {
-
-        const {
-            userId
-        } = req.session;
-
-        memosDAO.getAllMemos((err, docs) => {
-            if (err) return next(err);
-            return res.render("memos", {
-                memosList: docs,
-                userId: userId,
-                environmentalScripts
-            });
-        });
-    };
-
+    memosDAO.getAllMemos(userId, (err, docs) => {
+      if (err) return next(err);
+      return res.render("memos", {
+        memosList: docs,
+        userId: userId,
+        environmentalScripts,
+      });
+    });
+  };
 }
 
 module.exports = MemosHandler;
