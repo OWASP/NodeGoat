4fa0232a60d3875bdf6df2baf2f42b91d0d67a3c
maytlead <MayT@leadforensics.com>
2023-10-25 15:18:03 +0200

    Fix: A2-1 Session Management
    - Use hashed password for seeding the data
    - Before saving the password, hash it
    - Compare the password with hash method

--- app/data/user-dao.js
+++ app/data/user-dao.js
@@ -22,12 +22,10 @@ function UserDAO(db) {
             firstName,
             lastName,
             benefitStartDate: this.getRandomFutureDate(),
-            password //received from request param
-            /*
+            // password //received from request param
             // Fix for A2-1 - Broken Auth
             // Stores password  in a safer way using one way encryption and salt hashing
             password: bcrypt.hashSync(password, bcrypt.genSaltSync())
-            */
         };
 
         // Add email if set
@@ -58,12 +56,10 @@ function UserDAO(db) {
 
         // Helper function to compare passwords
         const comparePassword = (fromDB, fromUser) => {
-            return fromDB === fromUser;
-            /*
+            // return fromDB === fromUser;
             // Fix for A2-Broken Auth
             // compares decrypted password stored in this.addUser()
             return bcrypt.compareSync(fromDB, fromUser);
-            */
         };
 
         // Callback to pass to MongoDB that validates a user document
