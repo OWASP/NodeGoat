1a8d308479643b6585ebcfef8c81d9630b5baa8d
maytlead <MayT@leadforensics.com>
2023-10-26 02:43:20 +0200

    Fix: A3 XSS Attack
    - Make cookie only accessible via http to prevent XSS attack

--- server.js
+++ server.js
@@ -82,22 +82,20 @@ MongoClient.connect(db, (err, db) => {
         secret: cookieSecret,
         // Both mandatory in Express v4
         saveUninitialized: true,
-        resave: true
+        resave: true,
         /*
         // Fix for A5 - Security MisConfig
         // Use generic cookie name
         key: "sessionId",
         */
 
-        /*
         // Fix for A3 - XSS
         // TODO: Add "maxAge"
         cookie: {
-            httpOnly: true
+            httpOnly: true,
             // Remember to start an HTTPS server to get this working
-            // secure: true
+            secure: true
         }
-        */
 
     }));
 
