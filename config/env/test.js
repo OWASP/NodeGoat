96f1ceaa5e854b22d07541cf6ee1101bb392f0b2
rsairam34 <rsairam34@gmail.com>
2024-02-13 13:24:49 +0530

    Update test.js

--- config/env/test.js
+++ config/env/test.js
@@ -6,3 +6,16 @@ module.exports = {
    zapApiKey: "v9dn0balpqas1pcc281tn5ood1",
    zapApiFeedbackSpeed: 5000 // Milliseconds.
 };
+
+const port = process.env.PORT || 4000;
+let db = process.env.MONGODB_URI || "mongodb://localhost:27017/nodegoat";
+
+module.exports = {
+    port,
+    db,
+    cookieSecret: "session_cookie_secret_key_here",
+    cryptoKey: "a_secure_key_for_crypto_here",
+    cryptoAlgo: "aes256",
+    hostName: "localhost",
+    environmentalScripts: []
+};
